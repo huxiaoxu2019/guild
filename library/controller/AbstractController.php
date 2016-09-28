49ff9657c63190e6b9d918352a3457b9b32541fb
huxu <admin@ihuxu.com>
2016-09-28 14:22:05 +0800

    modify the abstract controller

--- library/controller/AbstractController.php
+++ library/controller/AbstractController.php
@@ -23,9 +23,42 @@ abstract class AbstractController
 	 */
 	public function __construct()
 	{
+        /* Smarty */
 		$this->view = new \Smarty();
 		$this->view->caching = false;
 		$this->view->template_dir = SMARTY_TEMPLATE_DIR;
 		$this->view->compile_dir = SMARTY_COMPILE_DIR;
+
+		/* WEB */
+		if (APP_MODE == 'WEB') {
+            /* valid action name */
+			$actionName = ACTION_NAME;
+			if (!method_exists($this, ACTION_NAME)) {
+				die('Unknown page.');
+			}
+
+            /* valid some constants */
+            if (!check_app_version() || !check_build_version()) {
+                die('Unvalid param.');
+            }
+
+            /* call method */
+            $this->$actionName();
+        }
 	}
+
+    /**
+     * Redirect.
+     *
+     * @param $location string
+     * @param $params array
+     */
+    public function redirect($location, $params) {
+        if (APP_MODE != 'WEB') {
+            return;
+        }
+        $location = $location . '?' . http_build_query($params);
+        header("location:{$location}");
+        exit;
+    }
 }
