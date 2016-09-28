306c937dddcfec43a905f1dce63af01f50dcdaac
huxu <admin@ihuxu.com>
2016-09-28 14:34:01 +0800

    optimization of the sync class

--- library/util/Sync.php
+++ library/util/Sync.php
@@ -16,23 +16,17 @@ use Library\Util\Helper;
 
 class Sync
 {
-	
 	/**
 	 * Deploy.
 	 */
 	public static function deploy() {
 		$cmdPath  = Config::get('common.product.cmd_path');
-		$cmdPath = '/data1/www/htdocs/huati_build';
+		$passwd  = Config::get('common.build.sync_passwd');
+		$syncModule = Config::get("common.build.sync_module");
 
 		Helper::logLn(RUNTIME_LOG, "Pulling the last source code to server from the local cmd_path {$cmdPath}...");
-		$repository = Config::get('common.product.cmd_path');
-		$gitModel = new GitModel($repository);
-		$gitModel->pull();
-
 		Helper::logLn(RUNTIME_LOG, "Deploying the project to the server(s)...");
-		$syncModule = Config::get("common.build.sync_module");
-//		$ryncCmd  = "/usr/bin/rsync -vzrtopg --exclude-from '/root/run/huati_build/config/rsync_exclude_list' --password-file=/etc/rsyncd.passwd50  --delete --progress {$cmdPath}/ tongjian@10.73.15.50::{$syncModule}";
-		$ryncCmd  = "/usr/bin/rsync -vzrtopg --exclude-from '/root/run/huati_build/config/rsync_exclude_list' --password-file=/etc/rsyncd.passwd  --delete --progress {$cmdPath}/ root@10.210.241.151::{$syncModule}";
+		$ryncCmd  = "/usr/bin/rsync -vzrtopg --exclude-from '" . APP_PATH . "/config/rsync_exclude_list' --password-file={$passwd}  --delete --progress {$cmdPath}/ root@10.210.237.31::{$syncModule}";
 
 		Helper::logLn(RUNTIME_LOG, "rsync:{$ryncCmd}");
 		$output = <<<DEPLOY
